6233267d9b165ebc107aeed6f58e92bb5e51d289
dewandra <you@example.com>
2025-07-27 11:37:08 +0700

    setup tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <header class="bg-gray-800 text-white p-4">
+      <nav class="container mx-auto flex justify-between">
+        <div class="flex items-center space-x-4">
+          <router-link to="/" class="hover:text-gray-300">Home</router-link>
+          <router-link to="/dashboard" class="hover:text-gray-300">Dashboard</router-link>
+        </div>
+        <div class="flex items-center space-x-4">
+          <router-link to="/login" class="hover:text-gray-300">Login</router-link>
+          <router-link to="/register" class="hover:text-gray-300">Register</router-link>
+        </div>
+      </nav>
+    </header>
+    <main class="container mx-auto p-4">
+      <router-view />
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+</script>
